f2239c4af282cc687340b3baa933ea280fd8f1d0
piratetm <tvpiratetm@gmail.com>
2021-03-09 22:57:21 +0300

    new_commit

---
+++ python_bot.py
@@ -0,0 +1,16 @@
+import telebot
+bot = telebot.TeleBot('1658215806:AAETj5KBEi8Sv4wBnnrcKW7BY7zuuq9hoNk')
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+
+bot.polling(none_stop=True)
